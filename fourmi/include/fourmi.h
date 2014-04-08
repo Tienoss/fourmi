bed156708c5534ff2ab138f85e65cc3361f4f967
Etienne <etienne.jambet@gmail.com>
2014-04-08 15:11:31 +0200

    création projet base

---
+++ fourmi/include/fourmi.h
@@ -0,0 +1,14 @@
+#ifndef FOURMI_H
+#define FOURMI_H
+
+
+class fourmi
+{
+    public:
+        fourmi();
+        virtual ~fourmi();
+    protected:
+    private:
+};
+
+#endif // FOURMI_H
---
+++ fourmi/src/fourmi.cpp
@@ -0,0 +1,11 @@
+#include "fourmi.h"
+
+fourmi::fourmi()
+{
+    //ctor
+}
+
+fourmi::~fourmi()
+{
+    //dtor
+}
